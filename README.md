806361edee72d84850a14812538e881ffb686817
Andhy-Stephano <RandrialalatianAndhy@gmail.com>
2026-06-21 16:21:48 +0300

    SPRINT0: ajout du FrontControllerServlet et structure de base

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mini Spring Framework
+
+Framework Spring MVC simplifié réalisé dans le cadre du Projet S4.
+
+## SPRINT0
+- Création du FrontControllerServlet
+- Gestion des requêtes via doGet/doPost → processRequest
---
+++ src/servlet/FrontControllerServlet.java
@@ -0,0 +1,45 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FrontControllerServlet extends HttpServlet {
+
+    // Méthode centrale qui gère toutes les requêtes
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        // On récupère l'URL complète tapée par l'utilisateur
+        String urlDemandee = request.getRequestURL().toString();
+        
+        // On affiche le résultat directement sur l'écran du navigateur
+        out.println("<html>");
+        out.println("<head><title>Sprint 0</title></head>");
+        out.println("<body>");
+        out.println("<h1>[Sprint 0] Mon Framework Spring MVC</h1>");
+        out.println("<p>Le FrontControllerServlet a bien intercepté la requête.</p>");
+        out.println("<strong>L'URL détectée est :</strong> " + urlDemandee);
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}

db2a08153921e11c50a84e46e736c1cf3e5e187b
Andhy-Stephano <RandrialalatianAndhy@gmail.com>
2026-06-25 00:34:47 +0300

    SPRINT2: annotation @GetMapping et table de routage URL vers methode

---
+++ src/annotation/GetMapping.java
@@ -0,0 +1,14 @@
+package annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// Cette annotation se colle sur une METHODE (pas une classe)
+// Elle indique quelle URL cette methode doit traiter
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface GetMapping {
+    String value(); // l'URL a stocker, ex: "/emp/list"
+}
